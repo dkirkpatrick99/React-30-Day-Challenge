a9cf512e0b20c77333a7b55e4b9aa99dd7a3fd6e
Dalton Kirkpatrick <d.kirkpatrick99@gmail.com>
2021-11-19 12:50:20 -0600

    Finished day 12

--- src/App.js
+++ src/App.js
@@ -1,80 +1,329 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
+const options = [
+    {
+        value: '',
+        label: '-- Select Country--',
+    },
+    {
+        value: 'Finland',
+        label: 'Finland',
+    },
+    {
+        value: 'Sweden',
+        label: 'Sweden',
+    },
+    {
+        value: 'Norway',
+        label: 'Norway',
+    },
+    {
+        value: 'Denmark',
+        label: 'Denmark',
+    },
+]
+
+// mapping the options to list(array) of JSX options
+
+const selectOptions = options.map(({ value, label }) => (
+    <option value={value}> {label}</option>
+))
+
 class App extends Component {
+    // declaring state
     state = {
         firstName: '',
-        message: '',
-        key: '',
+        lastName: '',
+        email: '',
+        country: '',
+        tel: '',
+        dateOfBirth: '',
+        favoriteColor: '',
+        weight: '',
+        gender: '',
+        file: '',
+        bio: '',
+        skills: {
+            html: false,
+            css: false,
+            javascript: false,
+        },
+        touched: {
+            firstName: false,
+            lastName: false,
+        },
     }
-    handleClick = (e) => {
-        // e gives an event object
-        // check the value of e using console.log(e)
-        this.setState({
-            message: 'Welcome to the world of events',
-        })
-    }
-    // triggered whenever the mouse moves
-    handleMouseMove = (e) => {
-        this.setState({ message: 'mouse is moving' })
-    }
-    // to get value when an input field changes a value
     handleChange = (e) => {
-        this.setState({
-            firstName: e.target.value,
-            message: e.target.value,
-        })
-    }
+        /*
+         we can get the name and value like: e.target.name, e.target.value
+        Wwe can also destructure name and value from e.target
+        const name = e.target.name
+        const value = e.target.value
+        */
+        const { name, value, type, checked } = e.target
+        /*
+        [variablename] we can make a value stored in a certain variable could be a key for an object, in this case a key for the state
+        */
 
-    // to get keyboard key code when an input field is pressed
-    // it works with input and textarea
-    handleKeyPress = (e) => {
-        this.setState({
-            message:
-                `${e.target.value} has been pressed and the keycode is` + e.charCode,
-        })
+        if (type === 'checkbox') {
+            this.setState({
+                skills: { ...this.state.skills, [name]: checked },
+            })
+        } else if (type === 'file') {
+            this.setState({ [name]: e.target.files[0] })
+        } else {
+            this.setState({ [name]: value })
+        }
     }
-    // Blurring happens when a mouse leave an input field
     handleBlur = (e) => {
-        this.setState({ message: 'Input field has been blurred' })
+        const { name, value } = e.target
+        this.setState({ touched: { ...this.state.touched, [name]: true } })
     }
-    // This event triggers during a text copy
-    handleCopy = (e) => {
-        this.setState({
-            message: 'Using 30 Days Of React for commercial purpose is not allowed',
-        })
+    validate = () => {
+        // Object to collect error feedback and to display on the form
+        const errors = {
+            firstName: '',
+        }
+
+        if (
+            (this.state.touched.firstName && this.state.firstName.length < 3) ||
+            (this.state.touched.firstName && this.state.firstName.length > 12)
+        ) {
+            errors.firstName = 'First name must be between 2 and 12'
+        }
+        return errors
     }
+    handleSubmit = (e) => {
+        /*
+          e.preventDefault()
+          stops the default behavior of form element 
+          specifically refreshing of page
+          */
+        e.preventDefault()
+
+        const {
+            firstName,
+            lastName,
+            email,
+            country,
+            gender,
+            tel,
+            dateOfBirth,
+            favoriteColor,
+            weight,
+            bio,
+            file,
+            skills,
+        } = this.state
+
+        const formattedSkills = []
+        for (const key in skills) {
+            console.log(key)
+            if (skills[key]) {
+                formattedSkills.push(key.toUpperCase())
+            }
+        }
+        const data = {
+            firstName,
+            lastName,
+            email,
+            country,
+            gender,
+            tel,
+            dateOfBirth,
+            favoriteColor,
+            weight,
+            bio,
+            file,
+            skills: formattedSkills,
+        }
+        /*
+         the is the place where we connect backend api
+          to send the data to the database
+          */
+        console.log(data)
+    }
+
     render() {
+        // accessing the state value by destrutcturing the state
+        // the noValidate attribute on the form is to stop the HTML5 built-in validation
+
+        const { firstName } = this.validate()
         return (
-            <div>
-                <h1>Welcome to the World of Events</h1>
+            <div className='App'>
+                <h3>Add Student</h3>
+                <form onSubmit={this.handleSubmit} noValidate>
+                    <div className='row'>
+                        <div className='form-group'>
+                            <label htmlFor='firstName'>First Name </label>
+                            <input
+                                type='text'
+                                name='firstName'
+                                value={this.state.firstName}
+                                onChange={this.handleChange}
+                                onBlur={this.handleBlur}
+                                placeholder='First Name'
+                            /> <br />
+                            <small>{firstName}</small>
+                        </div>
+                        <div className='form-group'>
+                            <label htmlFor='lastName'>Last Name </label>
+                            <input
+                                type='text'
+                                name='lastName'
+                                value={this.state.lastName}
+                                onChange={this.handleChange}
+                                placeholder='Last Name'
+                            />
+                        </div>
+                        <div className='form-group'>
+                            <label htmlFor='email'>Email </label>
+                            <input
+                                type='email'
+                                name='email'
+                                value={this.state.email}
+                                onChange={this.handleChange}
+                                placeholder='Email'
+                            />
+                        </div>
+                    </div>
 
-                <button onClick={this.handleClick}>Click Me</button>
-                <button onMouseMove={this.handleMouseMove}>Move mouse on me</button>
-                <p onCopy={this.handleCopy}>
-                    Check copy right permission by copying this text
-        </p>
-
-                <p>{this.state.message}</p>
-                <label htmlFor=''> Test for onKeyPress Event: </label>
-                <input type='text' onKeyPress={this.handleKeyPress} />
-                <br />
-
-                <label htmlFor=''> Test for onBlur Event: </label>
-                <input type='text' onBlur={this.handleBlur} />
-
-                <form onSubmit={this.handleSubmit}>
-                    <div>
-                        <label htmlFor='firstName'>First Name: </label>
+                    <div className='form-group'>
+                        <label htmlFor='tel'>Telephone </label>
                         <input
+                            type='tel'
+                            name='tel'
+                            value={this.state.tel}
                             onChange={this.handleChange}
-                            name='firstName'
-                            value={this.state.value}
+                            placeholder='Tel'
+                        />
+                    </div>
+
+                    <div className='form-group'>
+                        <label htmlFor='dateOfBirth'>Date of birth </label>
+                        <input
+                            type='date'
+                            name='dateOfBirth'
+                            value={this.state.dateOfBirth}
+                            onChange={this.handleChange}
+                            placeholder='Date of Birth'
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor='favoriteColor'>Favorite Color</label>
+                        <input
+                            type='color'
+                            id='favoriteColor'
+                            name='favoriteColor'
+                            value={this.state.favoriteColor}
+                            onChange={this.handleChange}
+                            placeholder='Favorite Color'
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor='weight'>Weight </label>
+                        <input
+                            type='number'
+                            id='weight'
+                            name='weight'
+                            value={this.state.weight}
+                            onChange={this.handleChange}
+                            placeholder='Weight in Kg'
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor='country'>Country</label> <br />
+                        <select name='country' onChange={this.handleChange} id='country'>
+                            {selectOptions}
+                        </select>
+                    </div>
+
+                    <div>
+                        <p>Gender</p>
+                        <div>
+                            <input
+                                type='radio'
+                                id='female'
+                                name='gender'
+                                value='Female'
+                                onChange={this.handleChange}
+                                checked={this.state.gender === 'Female'}
+                            />
+                            <label htmlFor='female'>Female</label>
+                        </div>
+                        <div>
+                            <input
+                                id='male'
+                                type='radio'
+                                name='gender'
+                                value='Male'
+                                onChange={this.handleChange}
+                                checked={this.state.gender === 'Male'}
+                            />
+                            <label htmlFor='male'>Male</label>
+                        </div>
+                        <div>
+                            <input
+                                id='other'
+                                type='radio'
+                                name='gender'
+                                value='Other'
+                                onChange={this.handleChange}
+                                checked={this.state.gender === 'Other'}
+                            />
+                            <label htmlFor='other'>Other</label>
+                        </div>
+                    </div>
+
+                    <div>
+                        <p>Select your skills</p>
+                        <div>
+                            <input
+                                type='checkbox'
+                                id='html'
+                                name='html'
+                                onChange={this.handleChange}
+                            />
+                            <label htmlFor='html'>HTML</label>
+                        </div>
+                        <div>
+                            <input
+                                type='checkbox'
+                                id='css'
+                                name='css'
+                                onChange={this.handleChange}
+                            />
+                            <label htmlFor='css'>CSS</label>
+                        </div>
+                        <div>
+                            <input
+                                type='checkbox'
+                                id='javascript'
+                                name='javascript'
+                                onChange={this.handleChange}
+                            />
+                            <label htmlFor='javascript'>JavaScript</label>
+                        </div>
+                    </div>
+                    <div>
+                        <label htmlFor='bio'>Bio</label> <br />
+                        <textarea
+                            id='bio'
+                            name='bio'
+                            value={this.state.bio}
+                            onChange={this.handleChange}
+                            cols='120'
+                            rows='10'
+                            placeholder='Write about yourself ...'
                         />
                     </div>
 
                     <div>
-                        <input type='submit' value='Submit' />
+                        <input type='file' name='file' onChange={this.handleChange} />
+                    </div>
+                    <div>
+                        <button>Submit</button>
                     </div>
                 </form>
             </div>
@@ -82,4 +331,6 @@ class App extends Component {
     }
 }
 
+
 export default App
+
