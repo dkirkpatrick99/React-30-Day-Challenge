b7acbed5b11d16ea8a6bad2de3ec0a515e539a4c
Dalton Kirkpatrick <d.kirkpatrick99@gmail.com>
2021-11-19 14:07:46 -0600

    Finished day 15

--- src/App.js
+++ src/App.js
@@ -1,40 +1,98 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import ReactDOM from 'react-dom'
-import moment from 'moment'
-import {
-    TiSocialLinkedinCircular,
-    TiSocialGithubCircular,
-    TiSocialTwitterCircular,
-} from 'react-icons/ti'
+import React from 'react'
 
-const Footer = () => (
-    <footer>
-        <h3>30 Days Of React</h3>
-        <div>
-            <TiSocialLinkedinCircular />
-            <TiSocialGithubCircular />
-            <TiSocialTwitterCircular />
-        </div>
-        <div>
-            <small> Copyright &copy; {new Date().getFullYear()} </small>
-        </div>
-    </footer>
-)
+const Button = ({ onClick, text, style }) => {
+    return (
+        <button onClick={onClick} style={style}>
+            {text}
+        </button>
+    )
+}
 
+const buttonWithStyles = (CompParam, name = 'default') => {
+    const colors = [
+        {
+            name: 'default',
+            backgroundColor: '#e7e7e7',
+            color: '#000000',
+        },
+        {
+            name: 'react',
+            backgroundColor: '#61dbfb',
+            color: '#ffffff',
+        },
+        {
+            name: 'success',
+            backgroundColor: '#4CAF50',
+            color: '#ffffff',
+        },
+        {
+            name: 'info',
+            backgroundColor: '#2196F3',
+            color: '#ffffff',
+        },
+        {
+            name: 'warning',
+            backgroundColor: '#ff9800',
+            color: '#ffffff',
+        },
+        {
+            name: 'danger',
+            backgroundColor: '#f44336',
+            color: '#ffffff',
+        },
+    ]
+    const { backgroundColor, color } = colors.find((c) => c.name === name)
 
-class App extends Component {
+    const buttonStyles = {
+        backgroundColor,
+        padding: '10px 45px',
+        border: 'none',
+        borderRadius: 3,
+        margin: 3,
+        cursor: 'pointer',
+        fontSize: '1.25rem',
+        color,
+    }
+    return (props) => {
+        return <CompParam {...props} style={buttonStyles} />
+    }
+}
+
+const NewButton = buttonWithStyles(Button)
+const ReactButton = buttonWithStyles(Button, 'react')
+const InfoButton = buttonWithStyles(Button, 'info')
+const SuccessButton = buttonWithStyles(Button, 'success')
+const WarningButton = buttonWithStyles(Button, 'warning')
+const DangerButton = buttonWithStyles(Button, 'danger')
+
+class App extends React.Component {
     render() {
         return (
             <div className='App'>
-                <h1>How to use moment</h1>
-                <p>This challenge was started {moment('2020-10-01').fromNow()}</p>
-                <p>The challenge will be over in {moment('2020-10-30').fromNow()}</p>
-                <p>Today is {moment(new Date()).format('MMMM DD, YYYY HH:mm')}</p>
+                <Button text='No Style' onClick={() => alert('I am not styled yet')} />
+                <NewButton
+                    text='Styled Button'
+                    onClick={() => alert('I am the default style')}
+                />
+                <ReactButton text='React' onClick={() => alert('I have react color')} />
+                <InfoButton
+                    text='Info'
+                    onClick={() => alert('I am styled with info color')}
+                />
+                <SuccessButton text='Success' onClick={() => alert('I am successful')} />
+                <WarningButton
+                    text='Warning'
+                    onClick={() => alert('I warn you many times')}
+                />
+                <DangerButton
+                    text='Danger'
+                    onClick={() => alert('Oh no, you can not restore it')}
+                />
             </div>
         )
     }
 }
+
 
 
 
