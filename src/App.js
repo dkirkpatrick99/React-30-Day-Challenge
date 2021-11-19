d1b21ae5657faae30293cc03468374ebc956d5b4
Dalton Kirkpatrick <d.kirkpatrick99@gmail.com>
2021-11-19 14:44:08 -0600

    Finished day 17

--- src/App.js
+++ src/App.js
@@ -1,100 +1,61 @@
-import React from 'react'
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    NavLink,
+} from 'react-router-dom'
 
-const Button = ({ onClick, text, style }) => {
-    return (
-        <button onClick={onClick} style={style}>
-            {text}
-        </button>
-    )
-}
+// Home component
+const Home = <h1>Welcome Home</h1>
+// About component
+const About = <h1>About Us</h1>
+// Contact component
+const Contact = <h1>Contact us</h1>
+// Challenge component
+const Challenges = (
+    <div>
+        <h1>30 Days Of React Challenge</h1>
+    </div>
+)
+const NotFound = <h1>The page your looking for not found</h1>
+const Navbar = () => (
+    <ul>
+        <li>
+            <NavLink to='/'>Home</NavLink>
+        </li>
+        <li>
+            <NavLink to='/about'>About</NavLink>
+        </li>
+        <li>
+            <NavLink to='/contact'>Contact</NavLink>
+        </li>
+        <li>
+            <NavLink to='/challenges'>Challenges</NavLink>
+        </li>
+    </ul>
+)
 
-const buttonWithStyles = (CompParam, name = 'default') => {
-    const colors = [
-        {
-            name: 'default',
-            backgroundColor: '#e7e7e7',
-            color: '#000000',
-        },
-        {
-            name: 'react',
-            backgroundColor: '#61dbfb',
-            color: '#ffffff',
-        },
-        {
-            name: 'success',
-            backgroundColor: '#4CAF50',
-            color: '#ffffff',
-        },
-        {
-            name: 'info',
-            backgroundColor: '#2196F3',
-            color: '#ffffff',
-        },
-        {
-            name: 'warning',
-            backgroundColor: '#ff9800',
-            color: '#ffffff',
-        },
-        {
-            name: 'danger',
-            backgroundColor: '#f44336',
-            color: '#ffffff',
-        },
-    ]
-    const { backgroundColor, color } = colors.find((c) => c.name === name)
-
-    const buttonStyles = {
-        backgroundColor,
-        padding: '10px 45px',
-        border: 'none',
-        borderRadius: 3,
-        margin: 3,
-        cursor: 'pointer',
-        fontSize: '1.25rem',
-        color,
-    }
-    return (props) => {
-        return <CompParam {...props} style={buttonStyles} />
-    }
-}
-
-const NewButton = buttonWithStyles(Button)
-const ReactButton = buttonWithStyles(Button, 'react')
-const InfoButton = buttonWithStyles(Button, 'info')
-const SuccessButton = buttonWithStyles(Button, 'success')
-const WarningButton = buttonWithStyles(Button, 'warning')
-const DangerButton = buttonWithStyles(Button, 'danger')
-
-class App extends React.Component {
+class App extends Component {
     render() {
         return (
-            <div className='App'>
-                <Button text='No Style' onClick={() => alert('I am not styled yet')} />
-                <NewButton
-                    text='Styled Button'
-                    onClick={() => alert('I am the default style')}
-                />
-                <ReactButton text='React' onClick={() => alert('I have react color')} />
-                <InfoButton
-                    text='Info'
-                    onClick={() => alert('I am styled with info color')}
-                />
-                <SuccessButton text='Success' onClick={() => alert('I am successful')} />
-                <WarningButton
-                    text='Warning'
-                    onClick={() => alert('I warn you many times')}
-                />
-                <DangerButton
-                    text='Danger'
-                    onClick={() => alert('Oh no, you can not restore it')}
-                />
-            </div>
+            <Router>
+                <div className='App'>
+                    <Navbar />
+
+                    <Routes>
+                        <Route exact path='/about' element={About} />
+                        <Route exact path='/contact' element={Contact} />
+                        <Route exact path='/challenges' element={Challenges} />
+                        <Route exact path='/' element={Home} />
+                        <Route component={NotFound} />
+                    </Routes>
+                </div>
+            </Router>
         )
     }
 }
-
-
-
 
 export default App
 
